d4a52b2d971845675969a421e9a1d29f521b0579
PofeiShit <191543154@qq.com>
2018-12-02 20:54:15 +0800

    fix a type error

--- day1/interpreter.cpp
+++ day1/interpreter.cpp
@@ -2,17 +2,16 @@
 
 Token Interpreter::get_next_token()
 {
-	std::string text(text);
-
+	std::string Text(text);
 	// is self.pos index  past the end of the self.text ? if so, then return EOF token because there is no more input left to convert into tokens
-	if(pos > text.length() - 1)
+	if(pos > Text.length() - 1)
 		return Token(EOF_, None);
 
 	// get a character at the position self.pos and decide what token to create based on the single character
-	char current_char = text[pos];
+	char current_char = Text[pos];
 	// if the character is a digit then convert it to integer, create an INTEGER token, increment self.pos index to point to the next character after the digit, and return the INTEGER token
 	if(isdigit(current_char)){
-		Token token = Token(INTEGER, current_char - '0');
+		Token token = Token(INTEGER, current_char);
 		pos++;
 		return token;
 	}
@@ -37,7 +36,6 @@ int Interpreter::expr()
 {
 	// set current token to the first token taken from the input
 	current_token = get_next_token();
-
 	// we expect the current token to be a single-digit integer
 	Token left = current_token;
 	eat(INTEGER);
