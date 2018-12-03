672fce9dff3d15fda3b84539be7886bcce4e0d19
PofeiShit <191543154@qq.com>
2018-12-03 22:39:18 +0800

    homework day1

---
+++ day2/interpreter.h
@@ -0,0 +1,27 @@
+#ifndef INTERPRETER_H_
+#define INTERPRETER_H_
+#include "token.h"
+#include <stdio.h>
+#include <stdlib.h>
+class Interpreter
+{
+	public:
+		std::string text;
+		size_t pos;
+		Token current_token;
+
+	public:
+		Interpreter(){}
+		Interpreter(const std::string &_text):text(_text), pos(0){
+			current_token = Token();
+		}
+
+		Token get_next_token();
+		void eat(const std::string &token_type);
+		int expr();	
+		void error();
+		void skip_blank(char &current_char);
+		Token get_number(char &current_char);
+};
+
+#endif
---
+++ day2/types.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <string>
+#include <iostream>
+const std::string INTEGER = "INTEGER";
+const std::string PLUS = "PLUS";
+const std::string EOF_ = "EOF";
+const std::string SUB = "SUB";
+const char None = '\0';
+#endif
