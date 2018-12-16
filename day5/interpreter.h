9dfe37adf6f234cfbdfa09cf61194c76169b1bcc
PofeiShit <191543154@qq.com>
2018-12-16 11:28:06 +0800

    day 5

---
+++ day5/interpreter.h
@@ -0,0 +1,21 @@
+#ifndef INTERPRETER_H_
+#define INTERPRETER_H_
+#include "token.h"
+#include "lexer.h"
+class Interpreter
+{
+	public:
+		Token current_token;
+		Lexer lexer;
+
+	public:
+		Interpreter();
+		Interpreter(const Lexer _lexer);
+		void eat(const std::string &token_type);
+		void error();
+		int expr();	
+		int factor();
+		int term();
+};
+
+#endif
