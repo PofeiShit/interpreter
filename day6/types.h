dc21e1309927257a92155cea7e906b1301dff700
PofeiShit <191543154@qq.com>
2018-12-23 11:15:20 +0800

    day5 homework

---
+++ day6/types.h
@@ -0,0 +1,15 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <string>
+#include <iostream>
+const std::string INTEGER = "INTEGER";
+const std::string PLUS = "PLUS";
+const std::string EOF_ = "EOF";
+const std::string MINUS = "MINUS";
+const std::string MUL = "MUL";
+const std::string DIV = "DIV";
+const std::string LPAREN = "(";
+const std::string RPAREN = ")";
+const char None = '\0';
+#endif
