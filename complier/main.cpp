d0370e6f5c39d6052ac6fac7e54d2f4248a986dd
PofeiShit <191543154@qq.com>
2019-03-03 15:06:43 +0800

    part2 virtual machine

--- complier/main.cpp
+++ complier/main.cpp
@@ -45,7 +45,36 @@ int eval() {	// do nothing yet
 		else if(op == CALL) {*--sp = (int)(pc + 1); pc = (int*)*pc;}
 		else if(op == ENT)	{*--sp = (int)bp; bp = sp; sp = sp - *pc++;}
 		else if(op == ADJ) 	{sp = sp + *pc++;}
-		else if(op == LEV)	{sp = bp
+		else if(op == LEV)	{sp = bp; bp = (int*)*sp++; pc = (int*)*sp++;} // restor call frame and PC
+		else if(op == LEA)	{ax = (int)(bp + *pc++);}	// load address for arguments;
+		else if(op == OR)	ax = *sp++ | ax;
+		else if(op == XOR)	ax = *sp++ ^ ax;
+		else if(op == AND)	ax = *sp++ & ax;
+		else if(op == EQ)	ax = *sp++ == ax;
+		else if(op == NE)	ax = *sp++ != ax;
+		else if(op == LT) 	ax = *sp++ < ax;
+		else if(op == LE)	ax = *sp++ <= ax;
+		else if(op == GT)	ax = *sp++ > ax;
+		else if(op == GE)	ax = *sp++ >= ax;
+		else if(op == SHL)	ax = *sp++ << ax;
+		else if(op == SHR) 	ax = *sp++ >> ax;
+		else if(op == ADD)	ax = *sp++ + ax;
+		else if(op == SUB)	ax = *sp++ - ax;
+		else if(op == MUL)	ax = *sp++ * ax;
+		else if(op == DIV) 	ax = *sp++ / ax;
+		else if(op == MOD) 	ax = *sp++ % ax;
+		else if(op == EXIT)	{ printf("exit(%d)", *sp); return *sp;}
+		else if(op == OPEN) { ax = open((char *)sp[1], sp[0]);}
+		else if(op == CLOS)	{ ax = close(*sp);}
+		else if(op == READ)	{ ax = read(sp[2], (char*)sp[1], *sp);}
+		else if(op == PTRF) { tmp = sp + pc[1]; ax = printf((char*)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]);}
+		else if(op == MALC)	{ ax = (int)malloc(*sp);}
+		else if(op == MSET) { ax = (int)memset((char*)sp[2], sp[1], *sp);}
+		else if(op == MCMP) { ax = memcmp((char*)sp[2], (char*)sp[1], *sp);}
+		else {
+				printf("unknow instruction:%d\n", op);
+				return -1;
+		}
 	}
 	return 0;
 }
@@ -93,6 +122,18 @@ int main(int argc, char **argv)
 
 	bp = sp = (int*)((int)stack + poolsize);
 	ax = 0;
+
+	i = 0;
+	text[i++] = IMM;
+	text[i++] = 10;
+	text[i++] = PUSH;
+	text[i++] = IMM;
+	text[i++] = 20;
+	text[i++] = ADD;
+	text[i++] = PUSH;
+	text[i++] = EXIT;
+	pc = text;	
+
 	program();
 	return eval();
 }
